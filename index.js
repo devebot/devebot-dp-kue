4dd886b321e75d66dcbc0922da889c8f31d295fa
Pham Ngoc Hung <pnhung177@drupalex.net>
2016-04-08 14:57:13 +0700

    Create the kue@jobQueue object

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
-console.log('This is jobdapter');
+'use strict';
+
+var events = require('events');
+var util = require('util');
+
+var JobQueue = require('kue');
+
+var debug = require('debug');
+var debuglog = debug('jobdapter');
+
+var Service = function(params) {
+  debuglog(' + constructor start ...');
+  Service.super_.call(this);
+  
+  params = params || {};
+  
+  var redis_conf = params.redis || {};
+
+  var jobQueue = JobQueue.createQueue({
+    prefix: redis_conf.name || 'devebotjq',
+    redis: {
+      host: redis_conf.host,
+      port: redis_conf.port || 6379,
+      options: {
+        retry_strategy: function (opts) {
+          if (opts.error.code === 'ECONNREFUSED') {
+            return new Error('The server refused the connection');
+          }
+
+          if (opts.total_retry_time > 1000 * 60 * 60) {
+            return new Error('Retry time exhausted');
+          }
+
+          if (opts.times_connected > 10) {
+            return undefined; // End reconnecting with built in error
+          }
+
+          return Math.max(opts.attempt * 100, 3000); // reconnect after
+        }
+      }
+    }
+  });
+  
+  jobQueue.watchStuckJobs(1000);
+
+  this.getJobQueue = function() {
+    return jobQueue;
+  };
+
+  this.getServiceInfo = function() {
+    return {};
+  };
+
+  this.getServiceHelp = function() {
+    return {};
+  };
+
+  debuglog(' - constructor has finished');
+};
+
+Service.argumentSchema = {
+  "id": "jobdapter",
+  "type": "object",
+  "properties": {
+    "redis": {
+      "type": "object"
+    }
+  }
+};
+
+util.inherits(Service, events.EventEmitter);
+
+module.exports = Service;
